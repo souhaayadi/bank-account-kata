da32708cbd33980b9c1b2fc54c74ae85908639c8
ayadis <ayadisouhaaa@gmail.com>
2025-08-02 17:59:34 +0200

    test : add exception test when deposit with none existing account

--- src/test/java/bank/domain/useCase/AccountServiceTest.java
+++ src/test/java/bank/domain/useCase/AccountServiceTest.java
@@ -1,5 +1,6 @@
 package bank.domain.useCase;
 
+import com.example.bank.domain.exceptions.AccountNotFoundException;
 import com.example.bank.domain.model.Account;
 import com.example.bank.domain.port.AccountPort;
 import com.example.bank.domain.useCase.AccountService;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.nio.channels.AcceptPendingException;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -39,5 +41,17 @@ public class AccountServiceTest {
         assertEquals(BigDecimal.valueOf(100), account.getBalance());
         assertEquals(1, account.getTransactions().size());
         verify(repository).save(account);
+    }
+    @Test
+    void should_throw_exception_when_depositing_on_none_existing_account() {
+        // Given
+        when(repository.findById(accountId)).thenReturn(Optional.empty());
+
+        //when //then
+        assertThrows(AccountNotFoundException.class, () ->
+                service.deposit(accountId, BigDecimal.valueOf(100)));
+
+        verify(repository, never()).save(any());
+
     }
 }
