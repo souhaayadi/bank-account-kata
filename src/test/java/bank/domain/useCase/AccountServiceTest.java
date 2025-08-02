30f3a3f62a7b55eebb157ddc91576c44ed3f075b
ayadis <ayadisouhaaa@gmail.com>
2025-08-02 18:10:11 +0200

    test : add withdraw test + refacto

--- src/test/java/bank/domain/useCase/AccountServiceTest.java
+++ src/test/java/bank/domain/useCase/AccountServiceTest.java
@@ -5,10 +5,10 @@ import com.example.bank.domain.model.Account;
 import com.example.bank.domain.port.AccountPort;
 import com.example.bank.domain.useCase.AccountService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.nio.channels.AcceptPendingException;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -28,30 +28,80 @@ public class AccountServiceTest {
         accountId = UUID.randomUUID();
     }
 
-    @Test
-    void should_deposit_money_and_update_balance() {
-       // Given
-        Account account = new Account(accountId);
-        when(repository.findById(accountId)).thenReturn(Optional.of(account));
+    @Nested
+    class depositMoney {
+        @Test
+        void should_deposit_money_and_update_balance() {
+            // Given
+            Account account = new Account(accountId);
+            when(repository.findById(accountId)).thenReturn(Optional.of(account));
 
-        // When
-        service.deposit(accountId, BigDecimal.valueOf(100));
+            // When
+            service.deposit(accountId, BigDecimal.valueOf(100));
 
-        // Then
-        assertEquals(BigDecimal.valueOf(100), account.getBalance());
-        assertEquals(1, account.getTransactions().size());
-        verify(repository).save(account);
+            // Then
+            assertEquals(BigDecimal.valueOf(100), account.getBalance());
+            assertEquals(1, account.getTransactions().size());
+            verify(repository).save(account);
+        }
+
+        @Test
+        void should_throw_exception_when_depositing_on_none_existing_account() {
+            // Given
+            when(repository.findById(accountId)).thenReturn(Optional.empty());
+
+            //when //then
+            assertThrows(AccountNotFoundException.class, () ->
+                    service.deposit(accountId, BigDecimal.valueOf(100)));
+
+            verify(repository, never()).save(any());
+
+        }
     }
-    @Test
-    void should_throw_exception_when_depositing_on_none_existing_account() {
-        // Given
-        when(repository.findById(accountId)).thenReturn(Optional.empty());
 
-        //when //then
-        assertThrows(AccountNotFoundException.class, () ->
-                service.deposit(accountId, BigDecimal.valueOf(100)));
+    @Nested
+    class withdrawMoney {
+        @Test
+        void should_withdraw_money_and_update_balance() {
+            // Given
+            Account account = new Account(accountId, BigDecimal.valueOf(200), new ArrayList<>());
+            when(repository.findById(accountId)).thenReturn(Optional.of(account));
 
-        verify(repository, never()).save(any());
+            // When
+            service.withdraw(accountId, BigDecimal.valueOf(150));
 
+            // Then
+            assertEquals(BigDecimal.valueOf(50), account.getBalance());
+            assertEquals(1, account.getTransactions().size());
+            verify(repository).save(account);
+        }
+
+        @Test
+        void should_throw_exception_when_withdrawing_more_than_balance() {
+            // Given
+            Account account = new Account(accountId, BigDecimal.valueOf(100), new ArrayList<>());
+            when(repository.findById(accountId)).thenReturn(Optional.of(account));
+
+            // When / Then
+            assertThrows(IllegalArgumentException.class, () ->
+                    service.withdraw(accountId, BigDecimal.valueOf(150))
+            );
+
+            verify(repository, never()).save(any());
+        }
+
+
+        @Test
+        void should_throw_exception_when_withdrawing_on_nonexistent_account() {
+            // Given
+            when(repository.findById(accountId)).thenReturn(Optional.empty());
+
+            // When / Then
+            assertThrows(AccountNotFoundException.class, () ->
+                    service.withdraw(accountId, BigDecimal.valueOf(100))
+            );
+
+            verify(repository, never()).save(any());
+        }
     }
 }
