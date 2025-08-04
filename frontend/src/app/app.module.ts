9835261358364e7e0602598b549d020eec517dca
ayadis <ayadisouhaaa@gmail.com>
2025-08-04 03:18:01 +0200

    chore : init front-end

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateAccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
